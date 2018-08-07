58a89c614435b1a196388a399c959dea339088d0
Matthew Callens <callensmatt@gmail.com>
2018-08-07 14:24:06 -0400

    update: welcome message format

--- index.ts
+++ index.ts
@@ -3,17 +3,48 @@ import * as Discord from 'discord.js'
 
 const bot: Discord.Client = new Discord.Client()
 
-function welcomeMessage(): object {
-  const embed = new Discord.RichEmbed()
-    .setTitle('Welcome to United Operations Discord!')
-    .setAuthor(bot.user.username, bot.user.avatarURL)
-    .setColor('#B19E71')
-    .setDescription('Placeholder description...')
-    .setURL('forums.unitedoperations.net')
-    .addField('General Information', 'http://forums.unitedoperations.net/index.php/page/servers')
-
-  return { embed }
+interface EmbedMessageImage {
+  url: string
 }
+
+interface EmbedMessageField {
+  name: string
+  value: string
+}
+
+interface EmbedMessage {
+  color: number
+  title: string
+  url: string
+  description: string
+  thumbnail: EmbedMessageImage
+  image: EmbedMessageImage
+  fields: EmbedMessageField[]
+}
+
+const welcomeMessage = (): EmbedMessage => ({
+  color: 11640433, // Integer representation of UO color #B19E71
+  title: '**Welcome to United Operations Discord!**', // Bolded with markdown
+  url: 'http://forums.unitedoperations.net',
+  description: '*Placeholder description...*', // Italisized with markdown
+  thumbnail: {
+    url: 'https://units.arma3.com/groups/img/1222/vSClUszph6.png'
+  },
+  image: {
+    url:
+      'http://forums.unitedoperations.net/public/style_images/United_Operations___Animated/scooby_banner_bg.png'
+  },
+  fields: [
+    {
+      name: 'Server Information',
+      value: 'http://forums.unitedoperations.net/index.php/page/servers'
+    },
+    {
+      name: 'Charter',
+      value: 'http://www.unitedoperations.net/wiki/United_Operations_Charter'
+    }
+  ]
+})
 
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}`)
@@ -22,11 +53,11 @@ bot.on('ready', () => {
 bot.on('message', (msg: Discord.Message) => {
   if (msg.author.tag === bot.user.tag) return
 
-  if (msg.content === 'new_user_test') msg.reply(welcomeMessage())
+  if (msg.content === 'new_user_test') msg.channel.send({ embed: welcomeMessage() })
 })
 
 bot.on('guildMemberAdd', (member: Discord.GuildMember) => {
-  member.send(welcomeMessage())
+  member.send({ embed: welcomeMessage() })
 })
 
 bot.login(process.env.BOT_TOKEN)
