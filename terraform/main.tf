ca974b697dd6033af3b78e66bec60b499457d1ca
Matthew Callens <callensmatt@gmail.com>
2019-01-28 21:00:58 -0500

    update: terraform scripts

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,29 @@ provider "google" {
   project     = "${var.project_id}"
   region      = "${var.region}"
 }
+
+data "google_compute_instance" "instance" {
+  name = "${var.instance_name}"
+  zone = "${var.instance_zone}"
+}
+
+resource "null_resource" "docker" {
+  triggers = {
+    image_digest = "${sha1(file("../VERSION"))}"
+  }
+
+  connection {
+    host     = "${data.google_compute_instance.instance.network_interface.0.access_config.0.nat_ip}"
+    user     = "${var.ssh_user}"
+    password = "${var.ssh_password}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo ${var.ssh_password} | sudo -S docker pull ${var.docker_image_name}",
+      "sudo docker stop $(sudo docker ps -a -q)",
+      "sudo docker run --rm -d -e \"ANNOUNCE=true\" ${var.docker_image_name}",
+      "sudo docker rmi $(sudo docker images -q | awk 'FNR == 1')",
+    ]
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,28 @@ variable "region" {
   type        = "string"
   description = "GCP project region"
 }
+
+variable "instance_name" {
+  type        = "string"
+  description = "Name of the Compute Instance hosting to chatbot"
+}
+
+variable "instance_zone" {
+  type        = "string"
+  description = "Availability zone that the Compute Instance is deployed in"
+}
+
+variable "docker_image_name" {
+  type        = "string"
+  description = "Name of the image on the Docker Hub to create the image from"
+}
+
+variable "ssh_user" {
+  type        = "string"
+  description = "User to SSH into in the Compute Instance"
+}
+
+variable "ssh_password" {
+  type        = "string"
+  description = "Password for the SSH user in the Compute Instance"
+}
